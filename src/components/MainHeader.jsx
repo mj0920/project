ccec2474716371ae12a93ee7de7778ffe460ec2b
MJ <lmj7772@gmail.com>
2022-09-21 02:13:13 +0900

    배경에 동영상 넣기

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,6 +1,6 @@
 import {Link} from 'react-router-dom'
 import  Image  from '../images/main__header.png'
-
+import VideoBg from '../video/videoBg.mp4'
 
 
 const MainHeader = () => {
@@ -8,7 +8,13 @@ const MainHeader = () => {
    
 <header className='main__header'>
 <div className="container main__header-container">
+<div className='main__header-left'>
 
+  <div className='overlay'></div>
+  <video src={VideoBg} autoPlay loop muted ></video>
+  <div className='video1'></div>
+  <h1>welcome</h1>
+  <p>안녕안녕</p>
 <h4>모험의 시작</h4>
 <h1>전설의 시작</h1>
 <p>
@@ -19,11 +25,15 @@ const MainHeader = () => {
 <Link to="/plans" className='btn lg '>GET START</Link>
 
 </div>
-<div className='main__header-image'></div>
+
+
+<div className='main__header-right'>
+<div className='main__header-circle'></div>
+<div className='main__header-image'>
 <img src={Image} alt='Main Header Image'/>
-
-
-
+</div>
+</div>
+</div>
 
 
 </header>
--- src/pages/Notfound/NotFound.jsx
+++ src/pages/Notfound/NotFound.jsx
@@ -1,10 +1,12 @@
 import './notfound.css'
+import notfound from '../../images/notfound.gif'
+
 
 const NotFound = () => {
   return (
     <div >NotFound
 
- 
+ <img src={notfound} alt='notfound'></img>
 
 
 </div>
