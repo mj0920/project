73878de01462d4b1b1bd1831b62d36a095235d0a
MJ <lmj7772@gmail.com>
2022-09-21 02:50:43 +0900

    메인 css까지

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,6 +1,7 @@
 import {Link} from 'react-router-dom'
 import  Image  from '../images/main__header.png'
 import VideoBg from '../video/videoBg.mp4'
+import  ImageBg  from '../images/1.png'
 
 
 const MainHeader = () => {
@@ -10,11 +11,11 @@ const MainHeader = () => {
 <div className="container main__header-container">
 <div className='main__header-left'>
 
-  <div className='overlay'></div>
+  
   <video src={VideoBg} autoPlay loop muted ></video>
-  <div className='video1'></div>
-  <h1>welcome</h1>
-  <p>안녕안녕</p>
+  <div className='imageBg' ><img src={ImageBg} alt='사진'/></div>
+  
+
 <h4>모험의 시작</h4>
 <h1>전설의 시작</h1>
 <p>
