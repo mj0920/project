434d7e1b25afdf2a543a3bb69d1c2546e69a02fa
MJ <lmj7772@gmail.com>
2022-09-21 14:01:47 +0900

    프로그램스 진입

---
+++ src/components/Programs.jsx
@@ -0,0 +1,20 @@
+import {FaCrown} from 'react'
+import Icon from '..images/2.png'
+
+
+
+const Programs = () => {
+  return (
+<section className="Programs">
+<div className="container programs__container">
+<div className="programs_head"></div>
+<span ></span>
+
+
+
+</div>
+</section>
+  )
+}
+
+export default Programs
