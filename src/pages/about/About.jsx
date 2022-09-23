298e7959e04011541fbf3fbbc551287d2d3890b9
MJ <lmj7772@gmail.com>
2022-09-23 16:44:25 +0900

    about 시작

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,8 +1,18 @@
+import Header from '../../components/Header'
 import './about.css'
+import HeaderImage from '../../images/headerbg1.jpg'
+
+
+
 
 const About = () => {
   return (
-    <div>about</div>
+<>
+
+   <Header title= "About Us" image={HeaderImage}>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis corporis unde labore quae nostrum. Ipsam, quaerat? Eos aliquid quaerat quam molestias perspiciatis sit in ipsa fugit esse ducimus, illum at.
+   </Header>
+   </>
   )
 }
 
